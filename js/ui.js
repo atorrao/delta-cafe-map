03beafa266b6d679d1590222f173e8e80e029381
atorrao <andre@addapters.com>
2026-05-26 13:27:29 +0100

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -247,14 +247,14 @@ const UI = {
     /* Foto / avatar com botão de upload */
     html += '<div class="ph-photo-wrap">';
     if (photoData) {
-      html += '<img src="' + photoData + '" class="ph-photo" alt="Foto de perfil">';
+      html += '<img src="' + photoData + '" class="ph-photo" alt="Foto de perfil" id="ph-photo-img">';
     } else {
-      html += '<div class="ph-photo ph-photo-avatar">' + Gamification.getAvatarSVG(pts, u.selectedAvatar) + '</div>';
+      html += '<div class="ph-photo ph-photo-avatar" id="ph-photo-img">' + Gamification.getAvatarSVG(pts, u.selectedAvatar) + '</div>';
     }
-    html += '<label class="ph-photo-upload-btn" title="Alterar foto">';
+    html += '<label class="ph-photo-upload-btn" for="ph-photo-file" title="Alterar foto">';
     html += '  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" /></svg>';
-    html += '  <input type="file" accept="image/*" style="display:none;" onchange="UI._uploadPhoto(this)">';
     html += '</label>';
+    html += '<input type="file" id="ph-photo-file" accept="image/*" style="display:none;">';
     html += '</div>';
 
     /* Nome, nível, stats */
@@ -457,6 +457,14 @@ const UI = {
 
     document.getElementById('profile-body').innerHTML = html;
     UI.switchProfileTab(UI._activeProfileTab || 'pontos');
+
+    /* Attach photo upload event after DOM insert */
+    var fileInput = document.getElementById('ph-photo-file');
+    if (fileInput) {
+      fileInput.addEventListener('change', function() {
+        UI._uploadPhoto(this);
+      });
+    }
   },
 
   /* Upload de foto de perfil */
